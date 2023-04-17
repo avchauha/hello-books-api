a879c91723a880d3d75715fa880ee64051dc0321
Arshdeep Chauhan <avc.swe17@gmail.com>
2023-04-17 15:40:16 -0700

    Created new files in Books API

---
+++ app/routes.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, jsonify
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Hello Book", "A fantasy novel"),
+    Book(2, "Horrors of Flask", "A horror novel."),
+    Book(3, "Who dunder init?", "A suspense novel.")
+]
+
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods = ["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id" : book.id,
+            "title" : book.title,
+            "description" : book.description
+        })
+    return jsonify(books_response), 200
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return{
+                "id": book.id,
+                "title": book.title, 
+                "description": book.description,
+            }
+    return{"message":f"Book {book_id} not found"}, 404
