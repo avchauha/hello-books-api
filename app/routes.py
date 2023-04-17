333db6a26a3a1ae266ab532b671b1da09b03b6be
Arshdeep Chauhan <avc.swe17@gmail.com>
2023-04-17 16:07:03 -0700

    Added helper function and refactored error codes for invalid endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -14,6 +14,20 @@ books = [
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+def validate_book(book_id):
+    # handle invalid book_id, return 400
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"Book {book_id} is invalid"}, 400))
+    # search for book_id in data, return book 
+    for book in books:
+        if book.id == book_id:
+            return book
+    # return a 404 for non-existing book 
+
+    abort(make_response({"message":f"Book {book_id} is not found"}, 404))
+
 @books_bp.route("", methods = ["GET"])
 def handle_books():
     books_response = []
@@ -27,15 +41,11 @@ def handle_books():
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def handle_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        return{"message":f"Book {book_id} is invalid"}, 400
-    for book in books:
-        if book.id == book_id:
-            return{
-                "id": book.id,
-                "title": book.title, 
-                "description": book.description,
+    book = validate_book(book_id)
+
+    return{
+        "id": book.id,
+        "title": book.title, 
+        "description": book.description,
             }
-    return{"message":f"Book {book_id} is not found"}, 404
+    
